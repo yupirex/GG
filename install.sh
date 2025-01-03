280e3519ffc35ec48414ded6dd5e83fb136e689c
YupiRex <yupirex@gmail.com>
2025-01-03 20:03:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ pkg update
 pkg upgrade 
 pkg i -y python3 python-pip 
 pip3 install telethon
-
-#termux-setup-storage
-#termux-wake-lock
-
-# pkg i wget ncurses-utils python openssl
-
 pkg autoclean
 pkg clean
+termux-setup-storage
+termux-wake-lock
+
+# pkg i wget ncurses-utils openssl
+
+
